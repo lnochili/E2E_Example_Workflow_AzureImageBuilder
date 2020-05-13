94f972e12ccf61c262e33e36a05fe8f4a8c0bf46
Lalitha Nochili <lanochil@microsoft.com>
2020-05-13 14:10:41 +0530

    Update start-nodeapp.sh

--- start-nodeapp.sh
+++ start-nodeapp.sh
@@ -1,5 +1,5 @@
 
 cd /var/www/myapp
-"echo 'packer' | sudo -S mv /tmp/server.js .
-"echo 'packer' | sudo -S chmod ugo+x server.js
-"echo 'packer' | sudo -S npm  init --force
+"echo 'packer' | sudo -S mv /tmp/server.js . "
+"echo 'packer' | sudo -S chmod ugo+x server.js"
+"echo 'packer' | sudo -S npm  init --force"
