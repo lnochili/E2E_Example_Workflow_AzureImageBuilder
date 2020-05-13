3f5f783ca8021ee30b800834a2683cfc31dfdd88
Lalitha Nochili <lanochil@microsoft.com>
2020-05-13 16:02:52 +0530

    Update node-install.sh

--- node-install.sh
+++ node-install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #Install Node and npm
-"echo 'packer' | sudo -S apt update"
-"echo 'packer' | sudo -S apt install nodejs"
-"echo 'packer' | sudo -S apt install npm"
+echo 'packer' | sudo -S apt update
+echo 'packer' | sudo -S apt install nodejs
+echo 'packer' | sudo -S apt install npm
 
-"echo "Nodejs version is: "
-"echo 'packer' | sudo -S nodejs --version"
+echo "Nodejs version is: 
+echo 'packer' | sudo -S nodejs --version
 
-"echo 'packer' | sudo -S mkdir /var/www/myapp"
+echo 'packer' | sudo -S mkdir /var/www/myapp
