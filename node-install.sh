b0a01ef08a872e40ed9439a2e826b4ee2ae9a2d9
Lalitha Nochili <lanochil@microsoft.com>
2020-05-13 17:05:57 +0530

    Update node-install.sh
    commands without  sudo

--- node-install.sh
+++ node-install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #Install Node and npm
-echo 'packer' | sudo -S apt update
-echo 'packer' | sudo -S apt install nodejs
-echo 'packer' | sudo -S apt install npm
+apt update
+apt install nodejs
+apt install npm
 
 echo "Nodejs version is: 
 
 nodejs --version
 
-echo 'packer' | sudo -S mkdir /var/www/myapp
-exit(0)
+mkdir /var/www/myapp
+
