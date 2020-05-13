1050edee59cafc1cc8b73897af96306d907b114e
Lalitha Nochili <lanochil@microsoft.com>
2020-05-13 18:39:31 +0530

    Update node-install.sh

--- node-install.sh
+++ node-install.sh
@@ -5,9 +5,14 @@ apt update
 apt install nodejs
 apt install npm
 
-echo "Nodejs version is: 
+echo "Nodejs version is: "
 
 nodejs --version
 
-mkdir /var/www/myapp
+#create myapp directory only if does not exist
 
+if [!-d /var/www/myapp]; 
+then
+    mkdir -p /var/www/myapp;
+fi;
+exit (0)
