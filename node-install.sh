5057f333cbb33630593516166acd5d88f4392c73
Lalitha Nochili <lanochil@microsoft.com>
2020-05-14 12:05:01 +0530

    Update node-install.sh

--- node-install.sh
+++ node-install.sh
@@ -2,18 +2,16 @@
 #Install Node and npm
 
 apt-get -y  update
+apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
 apt-get install -y nodejs
 apt-get  install -y npm
-set print_text='Nodejs version is:' 
-echo $print_text
-nodejs --version
-set print_text='npm version is:'
-echo $print_text
-npm --version
+echo 'Nodejs version is: ' `nodejs --version`
+echo 'npm version is: ' `npm --version`
 
-if [ ! -d /var/www/myapp ]; 
+if [ ! -d /var/www/myapp ] 
 then
-    mkdir -p /var/www/myapp;
-fi;
+    echo 'Creating directory'
+    mkdir -p /var/www/myapp
+fi
 
